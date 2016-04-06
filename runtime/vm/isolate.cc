0211b250e83d92899f05fe088eca5a83274b7d7f
John McCutchan <johnmccutchan@google.com>
2016-04-06 13:57:35 -0700

    Add reload timeline events

--- runtime/vm/isolate.cc
+++ runtime/vm/isolate.cc
@@ -1054,6 +1054,10 @@ void Isolate::DoneLoading() {
 
 void Isolate::OnStackReload() {
   ASSERT(reload_context_ == NULL);
+  Thread* thread = Thread::Current();
+  TimelineDurationScope tds(thread,
+                            Timeline::GetIsolateStream(),
+                            "OnStackReload");
   ReloadSources(false);
   PauseEventHandler();
 }
